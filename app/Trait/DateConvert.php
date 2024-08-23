c4498a293b22323ce9209e8440643f66dec5e379
taha <tahangudev@gmail.com>
2024-08-23 12:21:43 -0400

    debug data convert trait

--- app/Trait/DateConvert.php
+++ app/Trait/DateConvert.php
@@ -7,6 +7,6 @@ use Morilog\Jalali\Jalalian;
 trait DateConvert
 {
     public function DateConvert($column='created_at'){
-        return Jalalian::forge($this->column)->format('H:i:s Y/m/d');
+        return Jalalian::forge($this->$column)->format('H:i:s Y/m/d');
     }
 }
